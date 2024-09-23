96ff7227693d89abf2fb7717ee46cad365ddb7b3
hopeson <hopesonogaga@gmail.com>
2024-09-23 11:13:23 +0100

    ui functionlaity

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Result from "./result";
 import Calculator from "./calculator";
+import UserContext from "../../context/userContext";
+import { useContext } from "react";
+import Prompt from "./prompt";
 export default function Main() {
+  const { data } = useContext(UserContext);
   return (
     <div className="w-screen h-screen flex  justify-center items-center ">
       <Calculator />
-      <Result />
+      {data ? <Result></Result> : <Prompt></Prompt>}
     </div>
   );
 }
